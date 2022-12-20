51ab6d18306b49c56d3ab96b8927c16a9f87b697
alexmet2 <alexplaystation2@yahoo.com>
2022-12-20 21:38:22 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTickets_Video_asp.net_core_MVCNET5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
