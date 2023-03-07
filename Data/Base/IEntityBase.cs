76222db0dd94505f2d94f9cb9ee065679d1edfc6
alexmet2 <alexplaystation2@yahoo.com>
2023-03-07 16:46:57 +0200

    Del

--- Data/Base/IEntityBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eTickets_Video_asp.net_core_MVCNET5.Data.Base
-{
-    public interface IEntityBase
-    {
-        int Id { get; set; }
-
-    }
-}
--- Data/Services/ICinemasService.cs
+++
@@ -1,9 +0,0 @@
-﻿using eTickets_Video_asp.net_core_MVCNET5.Data.Base;
-using eTickets_Video_asp.net_core_MVCNET5.Models;
-
-namespace eTickets_Video_asp.net_core_MVCNET5.Data.Services
-{
-    public interface ICinemasService : IEntityBaseRepository<Cinema>
-    {
-    }
-}
